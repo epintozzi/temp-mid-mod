1149fc5e98f51a515236398e828471cd931baf54
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-02-13 15:49:17 -0700

    Add test that user only sees own links

--- spec/features/links/user_can_submit_link_spec.rb
+++ spec/features/links/user_can_submit_link_spec.rb
@@ -25,4 +25,16 @@ describe "user submits link" do
 
     expect(page).to have_content("There was a problem. Url is not a valid URL. Please try again.")
   end
+  scenario "user only sees their own links" do
+    user = create(:user)
+    user_2 = create(:user, email: "new@email.com")
+    link = Link.create(url: "http://www.google.com", title: "Google", user_id: user.id)
+    link = Link.create(url: "http://www.facebook.com", title: "Facebook", user_id: user_2.id)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit root_path
+
+    expect(page).to have_content("Google")
+    expect(page).to_not have_content("Facebook")
+  end
 end
