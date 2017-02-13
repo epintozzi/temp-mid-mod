b76a95cd3d852c35f59ae6c2c71fb5bfebe3582c
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-02-13 16:52:59 -0700

    Update links as read

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::LinksController < ApplicationController
   def update
     @link = Link.find(params[:id])
     if @link.update_attributes(link_params)
+      # send message here
       render json: @link
     else
       render json: @link.errors.full_messages, status: 500
